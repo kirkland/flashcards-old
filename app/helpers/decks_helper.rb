7249868923ba6fda042cb0da875e2d094fea0de3
Rob Kaufman <rob.m.kaufman@gmail.com>
2010-03-15 20:24:15 -0400

    after episode 2

--- app/helpers/decks_helper.rb
+++ app/helpers/decks_helper.rb
@@ -1,2 +1,7 @@
 module DecksHelper
+  def add_card_link(name)
+    link_to_function name do |page|
+      page.insert_html :bottom, :cards, :partial => 'card', :object => Card.new  
+    end
+  end
 end
