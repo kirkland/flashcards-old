f9df8f4819c7fc714f6e97e21d91222d045bb168
Rob Kaufman <rob.m.kaufman@gmail.com>
2010-03-16 18:14:14 -0400

    works! end of episode 3

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+  attr_accessor :should_destroy
+  
+  def should_destroy?
+    should_destroy.to_i == 1
+  end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,9 +1,25 @@
 class Deck < ActiveRecord::Base
   has_many :cards
+  after_update :save_cards
 
   def card_attributes=(card_attributes)
     card_attributes.each do |attributes|
-      cards.build(attributes)
+      if attributes[:id].blank?
+        cards.build(attributes)
+      else
+        card = cards.detect { |c| c.id == attributes[:id].to_i }
+        card.attributes = attributes
+      end
+    end
+  end
+
+  def save_cards
+    cards.each do |c|
+      if c.should_destroy?
+        c.destroy
+      else
+        c.save(false)        
+      end
     end
   end
 end
