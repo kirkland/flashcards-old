35e484a0ceda02677c2cbfb9fa088f92b88620c1
Rob Kaufman <rob.m.kaufman@gmail.com>
2010-03-17 20:21:21 -0400

    can see front of first card, part way to revealing back of card

---
+++ public/javascripts/application.js
@@ -0,0 +1,16 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+function mark_for_destroy(element) {
+    $(element).next('.should_destroy').value = 1;
+    $(element).up('.card').hide();
+}
+
+function reveal_back() {
+    //    $(element).hide()
+    //    $(element).up().up('div').hide()
+    //    $(element).up().up('div').down('div').hide()
+    //    $('cardback').hide()
+    //    $('cardback').replace(@deck.cards[0].back) // doesn't work, no such variable. 
+    $('cardback').replace("this is the new text")
+}
