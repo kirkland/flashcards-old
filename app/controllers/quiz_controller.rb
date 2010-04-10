9e11c54ea3aec1f3fcfdf3032170fba401933a5c
Rob Kaufman <rob.m.kaufman@gmail.com>
2010-04-10 09:31:14 -0400

    clean up quiz/card view, get text centered vertically

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -13,6 +13,18 @@ class QuizController < ApplicationController
   def quiz_card
     if session[:quiz].has_more?
       @card = session[:quiz].next
+
+      if @card.back.length < 5
+        @backsize = "50px"
+      else
+        @backsize = "20px"
+      end
+      if @card.front.length < 5
+        @frontsize = "80px"
+      else
+        @frontsize = "20px"
+      end
+
     else
       redirect_to root_url
     end    
